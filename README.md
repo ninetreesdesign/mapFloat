8fcdab2752172ebda14de083cabd07cdc3e0e08e
ds <ntd.david@protonmail.com>
2019-07-24 21:48:22 -0700

    update descriptions and format

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# mapFloat
-A version of the map() function for Arduino that accepts float/double arguments rather than integers
+# mapFloat()
+scale the range of an input value to a new range.
+
+This is a version of the map() function for Arduino that accepts float/double arguments rather than integers
--- ds_mapFloat.ino
+++ ds_mapFloat.ino
@@ -1,35 +1,36 @@
 /*
- * Arduino map function only works for integers
- * A simple change of variable types allows a floating point version
- * 
+ * Arduino map() function only works for integers;
+ * By changing variable types, a floating point version is made.
+ *
  */
- 
- void setup() {
-  // Set up serial port
-  Serial.begin(57600);
-  while (!Serial && (millis() < 5000)) {} // program continues after a time even if a monitor window is never opened
-  Serial.println("\nPort initialized");
 
-  // test new mapping function
-  String str = "";
-  float x;
-  float y;
-  for ( int i = 0; i < 10; i++) {
-    x = (float)i * .5 - 0.2;
-    y = mapFloat(x, -0.41, 1.0, 4, -17.8);
-    str =  String(x) + ", " + String(y);
-    Serial.println(str);
-  }
+/// setup function
+void setup() {
+    // Set up serial port
+    Serial.begin(57600);
+    // the while loop is needed for devices with USB port for programming
+    while (!Serial && (millis() < 5000)) {} // program continues after time interval even if a monitor window is never opened
+    Serial.println("\nPort initialized.\n");
 
+    // test new mapping function
+    String str = "";
+    float x;
+    float y;
+    for ( int i = 0; i < 10; i++) {
+        x = (float)i * .5 - 0.2;
+        y = mapFloat(x, -1.0, 1.0, -100, 100);
+        str =  String(x) + ", " + String(y);
+        Serial.println(str);
+    }
 }
 
 
 void loop() {
-  ;
+    ;    // nothing
 }
 
 
 /// map one numerical span to another with floating point values
 double mapFloat (double x, double in_min, double in_max, double out_min, double out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
